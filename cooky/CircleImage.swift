8b06c6e63b61c0143200be60f8573260987016a2
airine <airine0119@gmail.com>
2020-08-01 01:09:40 +0800

    finish section5

---
+++ cooky/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  cooky
+//
+//  Created by Dr.Fat on 2020/8/1.
+//  Copyright © 2020 aaron-xin. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+        .overlay(
+            Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
